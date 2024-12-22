a70cbb900600f8635ed75f5957978d250844f570
Raghava Jagannatham <raghavajagannatham9@gmail.com>
2024-12-22 11:13:44 +0530

    task-1

--- project/apps/app/src/App.tsx
+++ project/apps/app/src/App.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from 'react'
 import { List } from 'ui'
 
 const api = "https://pokeapi.co/api/v2/pokemon?limit=151"
 
 const App = () => {
+  useEffect(() => {
+    const fetchPokemonList = async () => {
+      let response = await fetch(api);
+      let data = await response.json()
+      let names = data.results.map((pokemon: {name: string}) => pokemon.name)
+      console.log(names)
+    }
+    fetchPokemonList()
+
+  }, [])
   return (
   <>
     <h1>Pokemon list:</h1>
